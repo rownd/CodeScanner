4900ecc11634c4588bf5d9c0d9e5904c12842120
Matt Hamann <mhamann@rownd.io>
2022-09-15 23:36:20 -0400

    chore: update name for non-conflict w/ cocoapods

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodeScanner",
+    name: "CodeScanner_Rownd",
     platforms: [
       .iOS(.v13),
       .macOS(.v11)
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CodeScanner",
+            name: "CodeScanner_Rownd",
             targets: ["CodeScanner"]),
     ],
     dependencies: [
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CodeScanner",
+            name: "CodeScanner_Rownd",
             dependencies: []),
     ]
 )
